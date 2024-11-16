de3d6d70282885e6fa34b9fcb63fad7dabdc28bf
Madhan Kumar <madhankumar4195@gmail.com>
2024-11-16 12:59:56 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
-  "https://njuwstkfbfgbxvamrlxr.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qdXdzdGtmYmZnYnh2YW1ybHhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTgxODI0MDUsImV4cCI6MjAzMzc1ODQwNX0.ICDuwIlR-VVpaXSOaXN5oVA3JZ5hOrbrfB4jbjzgvtY"
+  "https://pyfusxatiafnfaesecsb.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB5ZnVzeGF0aWFmbmZhZXNlY3NiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3NDIwODksImV4cCI6MjA0NzMxODA4OX0.yjGKgMBMB_3Ku1pQxYa9EqZwVSmPkuvMUeKx1iimz7U"
 );
 
 export default supabase;
